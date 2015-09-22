78568f10ee65d3764d89a23223ded3b51613cd3c
Justin Zimmerman <zimmy130@gmail.com>
2015-09-22 19:58:17 -0400

    start parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,49 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  if(json === 'true') {
+        return true;
+    } else if(json === 'false'){
+        return false;
+    } else if(json === 'null'){
+        return null;
+    } else if( (json[0] === '"' && json[json.length - 1] === '"')||(json[0] === "'" && json[json.length - 1] === "'") ){
+        var output = '';
+        for(var i = 1; i < json.length - 1; i++){
+            output += json[i];
+        }
+        return output;
+    } else if(json[0] === '['){
+        if(json[1] === ']'){
+            return [];
+        }
+        json = json.slice(1, json.length - 1);
+        json = json.replace(/\s+/g, '');
+        if(json !== ''){
+            json = json.split(',');
+        }
+        for(var i = 0; i < json.length; i++){
+            return parseJSON(json);
+        }
+    } else if(json[0] === '{' && json[json.length - 1] === '}'){
+            if(json[1] === '}'){
+                return {};
+            }
+
+            json = json.slice(2, json.length - 2);
+            json = json.replace(/\s*/g, '');
+            json = json.replace(/["]*/g, '');
+            json = json.split(':');
+            var key = json[0];
+            var value = json[1];
+            var obj = {};
+            if(value !== ''){
+                obj[key] = value;
+            } else {
+                obj[key] = value;
+            }
+            return obj;
+    } else {
+        throw SyntaxError;
+    }
 };
